934d41a601ceeaeefa9fc4aa57a0028d86312018
qhuongng <nq.huong.2304@gmail.com>
2024-04-12 15:00:07 +0700

    Add project files.

---
+++ PaintApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PaintApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
