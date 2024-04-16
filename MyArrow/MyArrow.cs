958f9a7878dbec2e210adbf3f5564fedf232b7b3
qhuongng <nq.huong.2304@gmail.com>
2024-04-16 15:47:56 +0700

    Modify Arrow class to have accurate bounding box

--- MyArrow/MyArrow.cs
+++ MyArrow/MyArrow.cs
@@ -63,7 +63,7 @@ namespace MyArrow
 
         public UIElement Convert()
         {
-            // find the arrow shaft unit vector.
+            // find the arrow shaft unit vector
             double vx = _end.X - _start.X;
             double vy = _end.Y - _start.Y;
             double dist = (double)Math.Sqrt(vx * vx + vy * vy);
@@ -78,22 +78,36 @@ namespace MyArrow
             Point wing1 = new Point(_end.X + ax, _end.Y + ay);
             Point wing2 = new Point(_end.X - ay, _end.Y + ax);
 
+            // calculate the bounding box of the arrow
+            double minX = Math.Min(_start.X, Math.Min(_end.X, Math.Min(wing1.X, wing2.X)));
+            double minY = Math.Min(_start.Y, Math.Min(_end.Y, Math.Min(wing1.Y, wing2.Y)));
+            double maxX = Math.Max(_start.X, Math.Max(_end.X, Math.Max(wing1.X, wing2.X)));
+            double maxY = Math.Max(_start.Y, Math.Max(_end.Y, Math.Max(wing1.Y, wing2.Y)));
+
+            double width = maxX - minX;
+            double height = maxY - minY;
+
             // create the arrow shaft
             Line line = new Line
             {
-                X1 = _start.X,
-                Y1 = _start.Y,
-                X2 = _end.X,
-                Y2 = _end.Y,
+                X1 = _start.X - minX,
+                Y1 = _start.Y - minY,
+                X2 = _end.X - minX,
+                Y2 = _end.Y - minY,
                 Stroke = _stroke,
                 StrokeThickness = _strokeWidth
             };
 
+            if (_strokeDashArray != null)
+            {
+                line.StrokeDashArray = new DoubleCollection(_strokeDashArray);
+            }
+
             // create the arrowhead
             PathFigure arrowHeadPath = new PathFigure();
-            arrowHeadPath.StartPoint = wing1;
-            arrowHeadPath.Segments.Add(new LineSegment(_end, true));
-            arrowHeadPath.Segments.Add(new LineSegment(wing2, true));
+            arrowHeadPath.StartPoint = new Point(wing1.X - minX, wing1.Y - minY);
+            arrowHeadPath.Segments.Add(new LineSegment(new Point(_end.X - minX, _end.Y - minY), true));
+            arrowHeadPath.Segments.Add(new LineSegment(new Point(wing2.X - minX, wing2.Y - minY), true));
 
             PathGeometry arrowGeometry = new PathGeometry();
             arrowGeometry.Figures.Add(arrowHeadPath);
@@ -106,13 +120,31 @@ namespace MyArrow
             };
 
             // combine arrow line and arrowhead into a single container
-            Canvas container = new Canvas();
+            Canvas container = new Canvas
+            {
+                Width = width,
+                Height = height
+            };
+
             container.Children.Add(line);
             container.Children.Add(arrowHead);
 
-            if (_strokeDashArray != null)
+            if (_end.X >= _start.X)
             {
-                line.StrokeDashArray = new DoubleCollection(_strokeDashArray);
+                container.SetValue(Canvas.LeftProperty, _start.X);
+            }
+            else
+            {
+                container.SetValue(Canvas.LeftProperty, _start.X - width);
+            }
+
+            if (_end.Y >= _start.Y)
+            {
+                container.SetValue(Canvas.TopProperty, _start.Y);
+            }
+            else
+            {
+                container.SetValue(Canvas.TopProperty, _start.Y - height);
             }
 
             return container;
